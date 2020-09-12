310cff2a4960bde1cd7aa93a10ad1358d95df503
Aza Maulana <azamaulanaaa@gmail.com>
2020-09-13 01:05:25 +0700

    file management

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import Zippy from './hosting/zippy'
-import mirror from './mirror'
+import Zippy from './mirror/hosting/zippy'
+import mirror from './mirror/mirror'
 let hosting = new Zippy();
 
 let magnetUri = 'magnet:?xt=urn:btih:9BE5638EF78E7F385569CFF71094EEBDC1FD9107&dn=Minecraft%3A%20Pocket%20Edition%20v0.14.1%20Android&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce';
