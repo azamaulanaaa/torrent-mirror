38d815dd27347f47f29a60d654749a656868506d
Aza Maulana <azamaulanaaa@gmail.com>
2020-09-19 07:52:59 +0700

    rapiin lagi

--- index.ts
+++ index.ts
@@ -1,112 +1,30 @@
-const app = require('express')()
-const port = process.env.PORT || 3000
+import Zippy from './mirror/hosting/zippy';
+import Mirror from './mirror';
+const cliProgress = require('cli-progress');
 
-// create an error with .status. we
-// can then use the property in our
-// custom error handler (Connect repects this prop as well)
+const args = process.argv.slice(2);
 
-function error(status, msg) {
-    return new class extends Error{
-        status : number
-        constructor(status, message){
-            super(message)
-            this.status = status
-        }
-    }(status, msg)
-}
-
-// if we wanted to supply more than JSON, we could
-// use something similar to the content-negotiation
-// example.
-
-// here we validate the API key,
-// by mounting this middleware to /api
-// meaning only paths prefixed with "/api"
-// will cause this middleware to be invoked
-
-app.use('/api', function(req, res, next){
-    var key = req.query['api-key']
-
-    // key isn't present
-    if (!key) return next(error(400, 'api key required'))
-
-    // key is invalid
-    if (!~apiKeys.indexOf(key)) return next(error(401, 'invalid api key'))
-
-    // all good, store req.key for route access
-    req.key = key
-    next()
-})
-
-// map of valid api keys, typically mapped to
-// account info with some sort of database like redis.
-// api keys do _not_ serve as authentication, merely to
-// track API usage or help prevent malicious behavior etc.
-
-var apiKeys = ['foo', 'bar', 'baz']
-
-// these two objects will serve as our faux database
-
-var repos = [
-    { name: 'express', url: 'https://github.com/expressjs/express' },
-    { name: 'stylus', url: 'https://github.com/learnboost/stylus' },
-    { name: 'cluster', url: 'https://github.com/learnboost/cluster' }
-]
-
-var users = [
-    { name: 'tobi' },
-    { name: 'loki' },
-    { name: 'jane' },
-]
-
-var userRepos = {
-    tobi: [repos[0], repos[1]],
-    loki: [repos[1]],
-    jane: [repos[2]]
-}
-
-// we now can assume the api key is valid,
-// and simply expose the data
-
-// example: http://localhost:3000/api/users/?api-key=foo
-app.get('/api/users', function(req, res, next){
-    res.send(users)
-})
-
-// example: http://localhost:3000/api/repos/?api-key=foo
-app.get('/api/repos', function(req, res, next){
-    res.send(repos)
-})
-
-// example: http://localhost:3000/api/user/tobi/repos/?api-key=foo
-app.get('/api/user/:name/repos', function(req, res, next){
-    var name = req.params.name
-    var user = userRepos[name]
-
-    if (user) res.send(user)
-    else next()
-})
-
-// middleware with an arity of 4 are considered
-// error handling middleware. When you next(err)
-// it will be passed through the defined middleware
-// in order, but ONLY those with an arity of 4, ignoring
-// regular middleware.
-app.use(function(err, req, res, next){
-    // whatever you want here, feel free to populate
-    // properties on `err` to treat it differently in here.
-    res.status(err.status || 500)
-    res.send({ error: err.message })
-})
-
-// our custom JSON 404 middleware. Since it's placed last
-// it will be the last middleware called, if all others
-// invoke next() and do not respond.
-app.use(function(req, res){
-    res.status(404)
-    res.send({ error: "Lame, can't find that" })
-})
-
-app.listen(port, ()=>{
-    console.log(`listening at http://localhost:${port}`)
+args.map((uri)=>{
+    const multibar = new cliProgress.MultiBar({
+        format : '[{bar}] {statETA}s | {name}',
+        clearOnComplete: true,
+        autopadding : true,
+        hideCursor: true,
+        barsize: 10
+    }, cliProgress.Presets.shades_classic);
+    let bars = []
+    let mirror = new Mirror();
+    mirror.add(uri, new Zippy(), (results) =>{
+        multibar.stop();
+        mirror.destroy();
+        console.log(results);
+    },(stats) =>{
+        stats.forEach((stat, index)=>{
+            if(!stat.stat)
+                return 0
+            if(!bars[index])
+                bars[index] = multibar.create(100,0,{name: stat.name});
+            bars[index].update(stat.stat.percentage, {statETA : stat.stat.eta});
+        })
+    })
 })
--- mirror/index.ts
+++ mirror/index.ts
@@ -3,9 +3,6 @@ import {ReadStream} from 'fs'
 const FSChunkStore = require('fs-chunk-store')
 const progress = require('progress-stream')
 
-let prog = (size : number = 0, callback : Function) =>{
-    return progress({time : 1000, length: size}, (stat) => {callback(stat)})
-}
 
 interface IMirrorResult{
     name : string
@@ -17,78 +14,101 @@ interface IMirrorStats{
     stat : any
 }
 
-type TMirrorCallback = (result: Array<IMirrorResult>) => void
-type TMirrorProgress = (stats: Array<IMirrorStats>) => void
 
-export default async function(sourceTorrent, hosting, callback : TMirrorCallback, progress : TMirrorProgress = ()=>{}){
-    const downloadDir = './download/'
-    let results : Array<IMirrorResult> = []
-    let stats : Array<IMirrorStats> = []
-    let torrentClient = new WebTorrent()
-    let chunk
-    
-    torrentClient.on('torrent', function(torrent){
-        console.log('Download : ' + torrent.name)
-        let uploads = torrent.files.map(function(file){
-            return new Promise(async resolve => {
-                let result : IMirrorResult = {
-                    name : file.name,
-                    uri : []
-                }
+export default class{
+    torrentClient
+    opt = {
+        downloadDir : './download'
+    }
+    constructor(opt? : object){
+        if(!opt) opt = {}
+        // this.opt = Object.assign(this.opt, opt)
+        this.torrentClient = new WebTorrent()
+    }
 
-                let uploadFn = async (name:string, stream:ReadStream, length : number) =>{
-                    let stat = {
-                        name : name,
-                        stat : null
-                    }
-                    stats.push(stat)
-                    let statsIndex = stats.indexOf(stat)
-                    await hosting.upload({
-                        fs : stream
-                        .pipe(prog(length, (stat)=>{
-                            stats[statsIndex].stat = stat
-                            progress(stats)
-                        }))
-                        ,fileName : stat.name
-                    }).then(url => {
-                        result.uri.push(url)
-                    })
-                }
+    private prog(size : number = 0, callback : Function){
+        return progress({time : 1000, length: size}, (stat) => {callback(stat)})
+    }
 
-                if(file.length > hosting.maxSize){
-                    for(let i=0;i * hosting.maxSize < file.length;i++){
-                        let startPos = i * hosting.maxSize
-                        let remainingSize = file.length - startPos
-                        let partSize = (hosting.maxSize < remainingSize) ? hosting.maxSize : remainingSize
-                        let partName = file.name + '.' + ('000' + (i+1)).slice(-3)
-                        await uploadFn(partName, file.createReadStream({
-                            start : startPos,
-                            end : startPos + partSize,
-                        }), partSize)
-                        
-                    }
-                }else{
-                    let fileStream = file.createReadStream()
-                    await uploadFn(file.name, fileStream, file.length)
-                }
-                resolve()
-            })
-        })
-        Promise.all(uploads).then( function(){   
-            torrentClient.destroy((err) =>{
-                if (err) throw err
-                chunk.destroy()
-                callback(results)
-            })         
-        })
-    })
-    torrentClient.add(sourceTorrent, {
-        store : function(chunkLength, storeOpts){
-            chunk = new FSChunkStore(chunkLength, {
-                path : downloadDir + storeOpts.torrent.infoHash,
+    async add(sourceTorrent, hosting, callback? : (result: Array<IMirrorResult>) => void, progress? : (stats: Array<IMirrorStats>) => void){
+        if(!callback) callback = ()=>{}
+        if(!progress) progress = ()=>{}
+
+        let results : Array<IMirrorResult> = []
+        let stats : Array<IMirrorStats> = []
+
+        let chunkStore = (chunkLength: number, storeOpts) => {
+            let chunk = new FSChunkStore(chunkLength, {
+                path : this.opt.downloadDir + '/' + storeOpts.torrent.infoHash,
                 length : storeOpts.length
             })
             return chunk
         }
-    })
+        let prog = this.prog
+        
+        this.torrentClient.add(sourceTorrent, {
+                store : function(chuckLength, storeOpts){return chunkStore(chuckLength, storeOpts)}
+            }, 
+            function(torrent){
+                console.log('Download : ' + torrent.name)
+                let uploads = torrent.files.map(function(file){
+                    return new Promise(async resolve => {
+                        let result : IMirrorResult = {
+                            name : file.name,
+                            uri : []
+                        }
+        
+                        let uploadFn = async (name:string, stream:ReadStream, length : number) =>{
+                            let stat = {
+                                name : name,
+                                stat : null
+                            }
+                            stats.push(stat)
+                            let statsIndex = stats.indexOf(stat)
+                            await hosting.upload({
+                                fs : stream
+                                .pipe(prog(length, (stat)=>{
+                                    stats[statsIndex].stat = stat
+                                    progress(stats)
+                                }))
+                                ,fileName : stat.name
+                            }).then(url => {
+                                result.uri.push(url)
+                            })
+                        }
+        
+                        if(file.length > hosting.maxSize){
+                            for(let i=0;i * hosting.maxSize < file.length;i++){
+                                let startPos = i * hosting.maxSize
+                                let remainingSize = file.length - startPos
+                                let partSize = (hosting.maxSize < remainingSize) ? hosting.maxSize : remainingSize
+                                let partName = file.name + '.' + ('000' + (i+1)).slice(-3)
+                                await uploadFn(partName, file.createReadStream({
+                                    start : startPos,
+                                    end : startPos + partSize,
+                                }), partSize)
+                                
+                            }
+                        }else{
+                            let fileStream = file.createReadStream()
+                            await uploadFn(file.name, fileStream, file.length)
+                        }
+                        resolve()
+                    })
+                })
+                Promise.all(uploads).then(() => {
+                    torrent.pause()
+                    torrent.destroy({destroyStore:true}, ()=>{
+                        callback(results)
+                    })
+                })
+            }
+        )
+    }
+
+    destroy(calbback? : (err) => void){         
+        this.torrentClient.destroy((err) =>{
+            calbback(err)
+        })         
+    }
 }
